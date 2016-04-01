0f47728ff6c69aea0409d2831c0d64de04273384
FWD <fightwithdogma@users.noreply.github.com>
2016-04-01 23:00:33 +0200

    Added timer in the bottom of the chat

--- grobot.user.js
+++ grobot.user.js
@@ -10,12 +10,31 @@
 
 (function() {
     'use strict';
-setInterval(function() {$(".robin-chat--vote-increase").click();}, 1000);
+
+$('#robinSendMessage').append('<div id="customCounter"></div>');
+setInterval(function() {
+    $(".robin-chat--vote-increase").click();
+    var a = $('span.robin-message--message:nth-child(2)').text().split(" ");
+    a.pop();
+    a = a.pop();
+    if (a == "end") {
+        a = 1;
+    }
+    var m = (a - Math.floor((Date.now() - new Date($("div.robin-message:nth-child(1) > time:nth-child(1)").attr("datetime"))) / 1000 / 60));
+    var s = (a - (Date.now() - new Date($("div.robin-message:nth-child(1) > time:nth-child(1)").attr("datetime"))) / 1000 / 60);
+    s = s - Math.floor(s);
+    s = Math.floor(s * 60);
+
+    $('#customCounter').html(a+' minutes '+s+' seconds remaining');
+}, 1000);
 setInterval(function() {$('#robinChatMessageList').html($('<div>').append($("div.robin-message:nth-child(1)").clone()).html()+$('<div>').append($("div.robin-message:nth-child(2)").clone()).html());}, 1000*60*10);
 setInterval(function() {
     var a = $('span.robin-message--message:nth-child(2)').text().split(" ");
     a.pop();
     a = a.pop();
+    if (a == "end") {
+        a = 1;
+    }
     $('#robinChatMessageList').append('<div class="robin-message robin--flair-class--no-flair robin--message-class--message robin--user-class--system"><time class="robin-message--timestamp" datetime="'+Date.now()+'">custom - log</time><span class="robin-message--from robin--username">[robin]</span><span class="robin-message--message">'+$(".robin-room-participant").length+" "+$(".robin-user-list-overflow-indicator").text()+' participants in the room, '+ (a - Math.floor((Date.now() - new Date($("div.robin-message:nth-child(1) > time:nth-child(1)").attr("datetime"))) / 1000 / 60))+' minutes remaining</span></div>');
 }, 1000*60*3);
 })();
